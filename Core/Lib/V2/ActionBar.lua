714fc6e0e3fb1589d069bac1f4e166383da9e613
Tony Lagnada <deloyz@gmail.com>
2023-06-15 15:05:13 -0700

    ActionBar.lua updates

--- Core/Lib/V2/ActionBar.lua
+++ Core/Lib/V2/ActionBar.lua
@@ -3,12 +3,13 @@ local _, ns = ...
 local O, GC, M, LibStub = ns.O, ns.O.GlobalConstants, ns.M, ns.O.LibStub
 local p = ns.O.Logger:NewLogger('ActionBar')
 
---- @alias ActionBar ActionBarMixin | _Frame
---- @class ActionBarMixin
-local L = {}
-ABP_ActionBarMixin = L
+-- TODO: Not sure what the role of this file yet
 
----@param o ActionBarMixin | _Frame
+--- @alias ActionBar _ActionBar | _Frame
+--- @class _ActionBar
+local L = {}
+
+---@param o _ActionBar | _Frame
 local function PropsAndMethods(o)
     -- todo
 end; PropsAndMethods(L)
